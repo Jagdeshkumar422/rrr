db682713d287b9fc5a497bef3d9ec511e50c4b77
Jagdeshkumar422 <jk9876211@gmail.com>
2025-05-21 10:37:39 +0500

    sadasd

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb+srv://admin:admin@cluster0.jra3xep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb+srv://admin:admin@cluster0.gpyfrmg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
